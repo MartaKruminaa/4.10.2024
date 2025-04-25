7ea9dd8a24657d14c1838f86bfe9fae5c17dfafa
Peperysnow <156832627+Peperysnow@users.noreply.github.com>
2025-04-25 08:46:00 +0300

    m

---
+++ 28.03.2025/script.js
@@ -0,0 +1,21 @@
+
+function myfunction()
+{
+    let name= document.getElementById("input_name");
+    let surname= document.getElementById("input_surname");
+    let h1_name= document.getElementById("name");
+    let age= document.getElementById("input_age");
+    
+     h1_name.innerText="hello,"+ name.value + " " + surname.value;
+    alert(name.value + "is" + age.value + "y.o." );
+if(age.value<=0)
+{
+    alert("Error:icorrect age!");
+}
+
+
+
+
+
+
+
